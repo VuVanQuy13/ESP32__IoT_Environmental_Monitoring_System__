3d49a8a403f89d15dcd6612c0e1affdcb02050b3
VuVanQuy13 <vuvanquy13112004@gmail.com>
2026-09-15 15:19:15 +0700

    push

---
+++ src/tasks/communication/network_config.h
@@ -0,0 +1,9 @@
+#pragma once
+// Existing project configuration moved unchanged from main.cpp.
+#define BLYNK_TEMPLATE_ID "TMPL6Jz6zCaGZ"
+#define BLYNK_TEMPLATE_NAME "IOT Measure Sensor"
+#define BLYNK_AUTH_TOKEN "kNDkR1p17mJJ6lcBspS4ivnyDhQnqiA9"
+namespace networkConfig {
+const char* ssid = "SN44";
+const char* password = "mothaiba";
+}
